fd39944f052f2df48d47c1067fa15d479793b44c
Julio <julio.cezar@liferay.com>
2024-05-17 20:59:18 -0300

    estilizacao inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import "./App.css";
 
 function App() {
 
   return (
     <>
-      <h2>Teste</h2>
+      <Navbar />
+      <Outlet />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import {BiCameraMovie, BiSearchAlt2} from "react-icons/bi";
+
+const Navbar = () => {
+  return (
+    <nav id="navbar">
+        <h2> <Link to="/"><BiCameraMovie/>MoviesLib</Link> </h2>
+        <form>
+            <input type="text" placeholder='Buscar...' />
+             <button type='submit'>
+                <BiSearchAlt2/>
+             </button>
+        </form>
+    </nav>
+  )
+}
+
+export default Navbar
