efc7890d893cd53ea74a5914dafd1727e60ee47f
Julio <julio.cezar@liferay.com>
2024-05-17 20:32:17 -0300

    Criação das pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter , Routes, Route} from 'react-router-dom';
 
+import Home from "./pages/Home.jsx";
+import Movie from "./pages/Movie.jsx";
+import Search from "./pages/Search.jsx";
 
 import App from './App.jsx'
-
-
+import "./index.css";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -13,9 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
    <BrowserRouter>
    <Routes>
     <Route element={ <App />}>
-      {/* <Route path='/' element={<Home />} />
+      <Route path='/' element={<Home/>} />
       <Route path='/movie:id' element={<Movie />} />
-      <Route path='search' element={<Search />} /> */}
+      <Route path='search' element={<Search />} />
     </Route>
    </Routes>
    
