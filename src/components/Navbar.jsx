0eecd357f45b628e4c1bd658ab88ab08aef58742
Julio Cezar <julio.cezar@liferay.com>
2024-05-22 15:36:47 -0300

    Busca de filmes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,40 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {BiCameraMovie, BiSearchAlt2} from "react-icons/bi";
 
 import "./Navbar.css";
+import { useState } from 'react';
 
 const Navbar = () => {
+
+  const [search, setSearch] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!search){
+      return;
+    }
+
+    navigate(`/search?q=${search}`);
+    setSearch("");
+
+
+
+  }
+
+
   return (
     <nav id="navbar">
         <h2> <Link to="/"><BiCameraMovie/>MoviesLib</Link> </h2>
-        <form>
-            <input type="text" placeholder='Buscar...' />
+        <form onSubmit={handleSubmit}>
+            <input 
+              type="text" 
+              placeholder='Buscar...' 
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+              />
              <button type='submit'>
                 <BiSearchAlt2/>
              </button>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,43 @@
+import { useState, useEffect } from "react"
+import { useSearchParams } from "react-router-dom"
+import MovieCard from "../components/MovieCard"
+
+import "./MoviesGrid.css";
+
+
+const searchUrl = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Search = () => {
+
+  const [searchParams] = useSearchParams(); 
+
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get("q");
+
+
+  const getSearchedMovies = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setMovies(data?.results);
+  }
+ 
+  useEffect(()=>{
+    const searchQUeryUrl =  `${searchUrl}?${apiKey}&query=${query}`
+    getSearchedMovies(searchQUeryUrl);
+  }, [query])
+
   return (
-    <div>Search</div>
+    <div className="container">
+      <h2 className="title">Resultados para: <span className="query-text">{query}</span></h2>
+      <div className="movies-container">
+        {movies.length === 0 && <p> Carregando...</p>}
+        {movies.length > 0 && movies.map((movie) => 
+          <MovieCard key={movie.id} movie={movie}/>
+        )}
+        </div>
+    
+    </div>
   )
 }
 
